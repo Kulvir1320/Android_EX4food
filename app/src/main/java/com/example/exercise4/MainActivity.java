819cb582f4cd2ba95f05aab6edfb275206a2363d
Kulvir Dhillon <kulvirdhillon240@gmail.com>
2019-12-03 18:32:47 -0500

    - learning how to commit

---
+++ app/src/main/java/com/example/exercise4/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.exercise4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Spinner khana = findViewById(R.id.food);
+        EditText paisa = findViewById(R.id.price);
+
+        khana.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+    }
+
+
+}
